52ee7c314e9184c3d4575dbe996c37cfedbd6b7d
Lucy Tomas <lucy.tomas@get10up.com>
2022-10-03 10:44:06 +0200

    add circleci workflow

---
+++ mystore/mystore-frontend/bin/deploy.sh
@@ -0,0 +1,2 @@
+aws s3 cp --recursive --acl public-read ./dist s3://mystore-angular-lucymtc/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./dist/index.html s3://mystore-angular-lucymtc/
